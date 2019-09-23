a21b92dafa1c8581c14452b6ad6336d6fe0df46d
Semjon Greef <semjon.greef@gmail.com>
2019-09-23 21:40:57 +0300

    faq wip.

--- sub.js
+++ sub.js
@@ -1,38 +1,18 @@
-window.addEventListener("load", function(){
-  function changeBackground() {
-    let hue = 179;
-    let s = Math.floor(Math.random() * (40 - 10)) + 10;
-    let l = Math.floor(Math.random() * (40 - 10)) + 10;
-    document.body.parentNode.style.backgroundColor = 'hsl('+hue+', '+s+'%, '+l+'%)';
-  }
-  // changeBackground();
-  // setInterval(changeBackground, 7000);
 
-  const [node] = document.querySelectorAll(".headline-subtitle");
-  let html = "";
-  const text = node.innerText;
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] == " ") html += text[i];
-    else html += "<span>" + text[i] + "</span>";
-  }
-  node.innerHTML = html;
-  let letters = document.querySelectorAll(".headline-subtitle span");
-  let counter = 0;
-  particlesJS.load('particles-js', 'particles.json');
+window.addEventListener('load', () => {
+  const acc = document.getElementsByClassName('accordion');
 
-  const t = setInterval(() => {
-    if (counter < letters.length) {
-      let searching = true;
-      while(searching) {
-        const index = Math.floor(Math.random() * letters.length);
-        if (letters[index] && !letters[index].classList.contains('show-letter')) {
-          letters[index].classList.add('show-letter');
-          searching = false;
-        }
+  for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener('click', function () {
+      this.classList.toggle('active');
+
+      const panel = this.nextElementSibling;
+      if (panel.style.display === 'block') {
+        panel.style.display = 'none';
+      } else {
+        panel.style.display = 'block';
       }
-      counter++;
-    } else {
-      clearInterval(t);
-    }
-  }, 30)
+    });
+  }
 });
+
